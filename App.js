573f107cdd663e06b45fbc40b8c8f670f232a580
Bindu Anand <137068272+binduanand@users.noreply.github.com>
2025-03-15 13:44:00 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import RecipeSearch from "./RecipeSearch"; // Importing the RecipeSearch component
+import "./styles.css"; // Importing the CSS file
+
+function App() {
+  return (
+    <div>
+      <RecipeSearch />
+    </div>
+  );
+}
+
+export default App;
